b499687adf844e76b7e885674a154d838a34f4ef
Hilquias Abias <hilquias.dev@gmail.com>
2022-02-20 21:46:24 -0300

    alteraçoes na questao 3

--- questaoTres.js
+++ questaoTres.js
@@ -2,42 +2,38 @@ function howManyAnagrams(str) {
   let anagramsCount = 0;
 
   const isAnagram = (a, b) => {
-    if (a.length === 1) {
-      if (a === b) return 1;
-    };
-    if (a.split('').sort().join('') === b.split('').sort().join('')) return 1;
-    return 0;
+    if (a.length === 1) { console.log(a, b); return a === b ? 1 : 0; };
+    const val = a.split('').reduce((acc, val) => {
+      acc += b.includes(val) ? 1 : 0;
+      return acc;
+    }, 0);
+
+    return val === a.length ? 1 : 0;
   };
 
   const arrayWithOneElement = str.split('');
   const arrayWithTwoElement = [];
+  const arrayWithThreeElement = [];
 
   for (let i = 0; i < arrayWithOneElement.length; i++) {
-    console.log(arrayWithOneElement[i]);
+    console.log(i, ' (i) ', arrayWithOneElement[i]);
     for (let j = i+1; j < arrayWithOneElement.length; j++) {
+      console.log(j, ' (j) ', arrayWithOneElement[j]);
       anagramsCount += isAnagram(arrayWithOneElement[i], arrayWithOneElement[j]);
-      console.log(isAnagram(arrayWithOneElement[i], arrayWithOneElement[j]));
-      arrayWithTwoElement.push(arrayWithOneElement[i] + arrayWithOneElement[j]);
-      console.log(arrayWithTwoElement[i]);
+      if (j + 2 !== arrayWithOneElement.length) {
+        arrayWithTwoElement.push(arrayWithOneElement[j-1] + arrayWithOneElement[j]);
+      }
     }
   }
 
   for (let i = 0; i < arrayWithTwoElement.length; i++) {
     for (let j = i+1; j < arrayWithTwoElement.length; j++) {
       anagramsCount += isAnagram(arrayWithTwoElement[i], arrayWithTwoElement[j]);
-      console.log(anagramsCount);
     };
   }
 
   return anagramsCount;
 }
 
-const test = howManyAnagrams('ovo');
-console.log(test);
-
-// const isAnagram = (a, b) => {
-//   if (a.length !== b.length) return 0;
-//   if (a.split('').sort().join('') === b.split('').sort().join('')) return 1;
-// };
-
-// console.log(isAnagram('ov', 'vo'));
+const tests = ['ifailuhkqq']; // 'ovo', 
+tests.forEach(test => console.log(howManyAnagrams(test)));
